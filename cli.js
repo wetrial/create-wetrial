5833273d09cd66aceb26ffafab92c9910923165a
xiexingen <1002275364@qq.com>
2020-06-24 17:02:29 +0800

    添加新版本检测

--- cli.js
+++ cli.js
@@ -1,11 +1,16 @@
 #!/usr/bin/env node
 
 const yParser = require('yargs-parser');
+const npmview = require('npmview');
 const semver = require('semver');
 const { existsSync } = require('fs');
 const { join } = require('path');
 const chalk = require('chalk');
 const run = require('./lib/run');
+
+// get local package name and version from package.json (or wherever)
+const pkgName = require('./package.json').name;
+const pkgVersion = require('./package.json').version;
 
 // print version and @local
 const args = yParser(process.argv.slice(2));
@@ -28,10 +33,45 @@ const { type, subtype, ssubtype } = args;
 delete args.type;
 delete args.subtype;
 delete args.ssubtype;
-run({
-  name,
-  type,
-  subtype,
-  ssubtype,
-  args,
+
+// get latest version on npm
+npmview(pkgName, (err, version, moduleInfo) => {
+  // compare to local version
+  if (semver.gt(version, pkgVersion)) {
+    console.log(
+      chalk.green(`
+                    **************************************************************
+                    *                     本地cli需要更新                         *
+                    *   .=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-.       *
+                    *    |                     ______                     |      *
+                    *    |                  .-"      "-.                  |      *
+                    *    |                 /            \                 |      *
+                    *    |     _          |              |          _     |      *
+                    *    |    ( \         |,  .-.  .-.  ,|         / )    |      *
+                    *    |     > "=._     | )(__/  \__)( |     _.=" <     |      *
+                    *    |    (_/"=._"=._ |/     /\     \| _.="_.="\_)    |      *
+                    *    |           "=._"(_     ^^     _)"_.="           |      *
+                    *    |               "=\__|IIIIII|__/="               |      *
+                    *    |              _.="| \IIIIII/ |"=._              |      *
+                    *    |    _     _.="_.="\          /"=._"=._     _    |      *
+                    *    |   ( \_.="_.="     '--------'     "=._"=._/ )   |      *
+                    *    |    > _.="                            "=._ <    |      *
+                    *    |   (_/                                    \_)   |      *
+                    *    |                                                |      *
+                    *    '-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-='      *
+                    *                                                            *
+                    *           本地当前版本：${pkgVersion},  最新版本：${version}              *
+                    *      执行 yarn global add create-wetrial 升级到最新版本      *
+                    **************************************************************
+      `),
+    );
+    return;
+  }
+  run({
+    name,
+    type,
+    subtype,
+    ssubtype,
+    args,
+  });
 });

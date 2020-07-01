e02f9409febf1e72131917cea77d0531ef3c37a9
xiexingen <1002275364@qq.com>
2020-07-01 11:21:33 +0800

    修改utils方法

--- lib/generators/app/templates/src/utils/index.ts
+++ lib/generators/app/templates/src/utils/index.ts
@@ -3,20 +3,20 @@ import { IKeyValue } from '@wetrial/core';
 import { getRoutePreFix } from '@wetrial/core/es/route-helper';
 
 /**
- *
+ * 调用history的 push，会自动追加前缀路由(如果应用使用了)
  * @param opt
  */
 export function historyPush(
   opt: string | { pathname: string; query?: IKeyValue<string>; search?: string },
 ) {
-  const pathnamePrefix = getRoutePreFix();
-  if (pathnamePrefix) {
+  const pathNamePrefix = getRoutePreFix();
+  if (pathNamePrefix) {
     if (typeof opt === 'string') {
-      history.push(`${pathnamePrefix}${opt}`);
+      history.push(`${pathNamePrefix}${opt}`);
     } else {
       history.push({
         ...opt,
-        pathname: `${pathnamePrefix}${opt.pathname}`,
+        pathname: `${pathNamePrefix}${opt.pathname}`,
       });
     }
   } else {
@@ -27,8 +27,18 @@ export function historyPush(
 /**
  * 获取带前缀的pathanme
  * @param pathname 路径
+ * @example  getFullPathName('/users/edit') ==> /xysis/users/edit
  */
 export function getFullPathName(pathname: string) {
-  const pathnamePrefix = getRoutePreFix();
-  return `${pathnamePrefix}${pathname}`;
+  const pathNamePrefix = getRoutePreFix();
+  return `${pathNamePrefix}${pathname}`;
+}
+
+/**
+ * 获取去掉前缀路由的路由
+ * @param fullPathName 全路由
+ */
+export function getSimplePathName(fullPathName: string) {
+  const pathNamePrefix = getRoutePreFix();
+  return fullPathName.replace(new RegExp(`^${pathNamePrefix}`, 'gi'), '');
 }

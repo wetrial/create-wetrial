602ad19308cb5d4426d3ffccf343fbbd1c7a255e
xiexingen <1002275364@qq.com>
2020-08-10 11:16:57 +0800

    预装模块

--- lib/generators/app/templates/config/routes.ts
+++ lib/generators/app/templates/config/routes.ts
@@ -1,6 +1,9 @@
 import { IBestAFSRoute } from '@umijs/plugin-layout';
-// import { MembershipShareRoutes } from '../src/modules/membership-share';
-<% if (external.isApp) { %>//<% } %>import {<%= external.upperCaseName %>Routes} from '../src/modules/<%= external.lowerCaseName %>';
+import { MembershipShareRoutes } from '../src/modules/membership-share';
+<% if (!external.isApp) { %>// import { CodeListManagementRoutes } from '../src/modules/code-list-management';
+<% if (theme==='platform-admin') { %>import { AuditLoggingPlatformAdminRoutes } from '../src/modules/audit-logging-platform-admin';<% } %>
+import { <%= external.upperCaseName %>Routes } from '../src/modules/<%= external.lowerCaseName %>';
+<% } %>
 
 // umi routes: https://umijs.org/zh/guide/router.html
 const routes: IBestAFSRoute[]= [
@@ -15,8 +18,10 @@ const routes: IBestAFSRoute[]= [
         path: '/',
         redirect: '/<%= external.trimThemeLowerCaseName %>',
       },
-      // ...MembershipShareRoutes,
-      <% if (external.isApp) { %>//<% } %> ...<%= external.upperCaseName %>Routes,
+      ...MembershipShareRoutes,
+      <% if (!external.isApp) { %>// ...CodeListManagementRoutes,
+      <% if (theme==='platform-admin') { %>...AuditLoggingPlatformAdminRoutes,<% } %>
+      ...<%= external.upperCaseName %>Routes,<% } %>
     ],
   },
 ];
